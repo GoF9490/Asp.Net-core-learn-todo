c5f23bc103df31e88705bd34b97a19f52d2353ea
GoF9490 <osis9490@gmail.com>
2023-11-14 23:11:49 +0900

    feat : BookService

---
+++ TodoApi/BookM/Service/BookService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using TodoApi.BookM.Models;
+
+namespace TodoApi.BookM.Service;
+
+public class BookService
+{
+    private readonly IMongoCollection<Book> _booksCollection;
+
+    // IOptions <- 생성자 주입 시켜주는 클래스?
+    public BookService(IOptions<BookStoreDatabaseSettings> settings)
+    {
+        MongoClient mongoClient = new MongoClient(settings.Value.ConnectionString);
+
+        IMongoDatabase mongoDatabase = mongoClient.GetDatabase(settings.Value.DatabaseName);
+
+        _booksCollection = mongoDatabase.GetCollection<Book>(settings.Value.BooksCollectionName);
+    }
+
+    public async Task<List<Book>> GetAsync() =>
+        await _booksCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Book?> GetAsync(string id) =>
+        await _booksCollection.Find(id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Book newBook) =>
+        await _booksCollection.InsertOneAsync(newBook);
+
+    public async Task UpdateAsync(string id, Book updateBook) =>
+        await _booksCollection.ReplaceOneAsync(book => book.Id == id, updateBook);
+
+    public async Task RemoveAsync(string id) =>
+        await _booksCollection.DeleteOneAsync(id);
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using TodoApi.BookM.Models;
+using TodoApi.BookM.Service;
 using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,13 @@ builder.Services.AddSwaggerGen();
 // 추가
 builder.Services.AddDbContext<TodoContext>(opt => 
     opt.UseInMemoryDatabase("TodoList"));
+
+// BookStoreDatabaseSettings 클래스에 appsetting.json 에서 작성한 BookStoreDatabase 데이터로 초기화?
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+// BookService를 싱글톤으로 DI에 등록. 스프링 빈 컨테이너 느낌인듯? 
+builder.Services.AddSingleton<BookService>();
 
 var app = builder.Build();
 
